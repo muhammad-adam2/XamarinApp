af7039f6526ca007406506a4c69ccc0fd3086841
muhammad-adam2 <muhammad_adam2@hotmail.co.za>
2019-01-21 15:54:15 +0200

    First Commit

---
+++ XamarinApp/ViewModels/TestViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace XamarinApp.ViewModels
+{
+    public class TestViewModel : BaseViewModel
+    {
+        public TestViewModel()
+        {
+            Title = "Test Page";
+        }
+    }
+}
---
+++ XamarinApp/Views/TestPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Xamarin.Forms;
+using XamarinApp.Resources;
+using XamarinApp.ViewModels;
+
+namespace XamarinApp.Views
+{
+    public partial class TestPage : ContentPage
+    {
+        TestViewModel viewModel;
+
+        public TestPage()
+        {
+            InitializeComponent();
+            //LabelTest.Text = AppResources.TestLabel;
+            BindingContext = viewModel = new TestViewModel();
+        }
+
+        void Handle_Clicked(object sender, EventArgs e)
+        {
+            AppResources.Culture = new CultureInfo("zu-ZA");
+            InitializeComponent();
+        }
+
+        void Handle_Clicked_1(object sender, EventArgs e)
+        {
+            AppResources.Culture = new CultureInfo("tn-ZA");
+            InitializeComponent();
+        }
+
+        void Handle_Clicked_2(object sender, EventArgs e)
+        {
+            AppResources.Culture = new CultureInfo("en-ZA");
+            InitializeComponent();
+        }
+    }
+}
