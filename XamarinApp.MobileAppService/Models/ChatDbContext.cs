6f11ba030e9b096483e94556c43ff4e9075ba356
Muhammad Adam <muhammad_adam2@hotmail.co.za>
2019-01-26 17:19:48 +0200

    Create DBContext

---
+++ XamarinApp.MobileAppService/Models/ChatDbContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace XamarinApp.MobileAppService.Models
+{
+    public partial class ChatDbContext : DbContext
+    {
+        public ChatDbContext()
+        {
+        }
+
+        public ChatDbContext(DbContextOptions<ChatDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Chats> Chats { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlite("Data Source=ChatDb.db");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.1-servicing-10028");
+
+            modelBuilder.Entity<Chats>(entity =>
+            {
+                entity.HasKey(e => e.ChatId);
+
+                entity.HasIndex(e => e.ChatId)
+                    .IsUnique();
+
+                entity.HasIndex(e => e.GroupId)
+                    .IsUnique();
+
+                entity.Property(e => e.ChatId).ValueGeneratedNever();
+            });
+        }
+    }
+}
---
+++ XamarinApp.MobileAppService/Models/Chats.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace XamarinApp.MobileAppService.Models
+{
+    public partial class Chats
+    {
+        public long ChatId { get; set; }
+        public long GroupId { get; set; }
+        public string ChatMessages { get; set; }
+    }
+}
